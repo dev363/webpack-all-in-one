c40ed6f2273b78343645b8e7665d9d0109c50a19
dev <satn363@gmail.com>
2020-09-05 18:07:51 +0530

    Add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,11 @@ const { mode } = require("webpack-nano/argv");
 const {
   MiniHtmlWebpackPlugin,
 } = require("mini-html-webpack-plugin");
+const { WebpackPluginServe } = require("webpack-plugin-serve");
 
 module.exports = {
+  watch: mode === "development",
+  entry: "./src",
   mode,
   plugins: [
     new MiniHtmlWebpackPlugin({
@@ -11,5 +14,20 @@ module.exports = {
         title: "Webpack demo",
       },
     }),
+    new WebpackPluginServe({
+      port: process.env.PORT || 8080,
+      static: "./dist",
+      liveReload: true,
+      waitForBuild: true,
+    }),
   ],
+  watchOptions: {
+    // Delay the rebuild after the first change
+    aggregateTimeout: 300,
+
+    // Poll using interval (in ms, accepts boolean too)
+    poll: 1000,
+    // Ignore node_modules to decrease CPU usage
+    ignored: /node_modules/,
+  }
 };
